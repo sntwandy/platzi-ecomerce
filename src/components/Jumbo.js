55b13b9ba4cc99e47968fe4d44f30723375ab4bc
sntwandy <sntwandy@gmail.com>
2020-08-28 16:28:18 -0400

    using plugin sharp for images, StaticQuery to use graphql to get the image

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
+import { Image } from "./"
 
 export default function Jumbo({ description }) {
   return (
@@ -8,6 +9,7 @@ export default function Jumbo({ description }) {
         <h2>Get the best exclusive and special of Platzi!</h2>
         <small>{description}</small>
       </div>
+      <Image name="icon" />
     </StyledJumbo>
   )
 }
