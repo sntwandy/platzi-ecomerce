098076c8148e8dce889cbe937d40273459bfe224
sntwandy <sntwandy@gmail.com>
2020-08-29 09:17:17 -0400

    Creating dynamics pages with createPage API from Gatsby inside the gatsby-node.js file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve(__dirname, "src/templates/Product.js")
+  const result = await graphql(`
+    query GET_PRODUCTS {
+      allStripePrice {
+        edges {
+          node {
+            id
+            unit_amount
+            product {
+              name
+              metadata {
+                description
+                img
+                wear
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+
+  result.data.allStripePrice.edges.forEach(({ node }) => {
+    createPage({
+      path: `${node.id}`,
+      component: productTemplate,
+    })
+  })
+}
--- src/components/Products.js
+++ src/components/Products.js
@@ -9,7 +9,7 @@ export default function Products({ products }) {
       <h2>Products</h2>
       <section>
         {products.map(({ node }) => {
-          const price = formatprice(node.price)
+          const price = formatprice(node.unit_amount)
           return (
             <article key={node.id}>
               <img src={node.product.metadata.img} alt={node.product.name} />
